b8d8ab339932add1052b912e1886d9864c484d43
Nico Schottelius <nico@wurzel.schottelius.org>
2016-03-22 07:23:36 +0100

    Updating for List reading

--- Main.java
+++ Main.java
@@ -8,23 +8,24 @@ import java.util.*; /* Scanner, Arraylist */
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String s;
-        String []lines;
-        int i = 0;
+        Scanner s = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
+        
+        while (s.hasNextLine()){
+            list.add(s.nextLine());
+        }
 
-
-        int nTests = Integer.parseInt(in.readLine());
-        System.out.println("Tests: " + Integer.toString(nTests));
-
-        lines = new String[nTests];
+        for(int i = 1; i < list.size(); i++) {
+            System.out.println("Line " + i + ": " + list.get(i));
+        }
+        Exercise e1 = new Exercise(list);
 
         /* Read & run tests */
-        while ((s = in.readLine()) != null && s.length() != 0) {
-            Exercise e1 = new Exercise();
+        /*        while ((s = in.readLine()) != null && s.length() != 0) {
+            Exercise e1 = new Exercise(s);
             System.out.println("Test " + i++);
-            System.out.println(e1.blum_median(s));
-        }        
+            System.out.println(e1.blum_median());
+            }*/
     }
 
 }
@@ -32,64 +33,85 @@ public class Main {
 class Exercise {
 
     private int maximum;
-    private int [] inputlist;
+
+    //    private int [] inputlist;
     private int elements;
     
-    public Exercise() {
-        maximum = 1000;
-        inputlist = new int[maximum];
-        elements = 0;
+    private String input;
+
+    ArrayList<ArrayList<Integer>> list_of_int_lists;
+
+    ArrayList <String> inputlist;
+    
+    public Exercise(ArrayList <String> list) {
+        // this.input = input;      /* input given to the program          */
+        this.inputlist = list;      /* input given to the program          */
+        maximum = 1000;          /* Maximum number of elements per line */
     }
 
-    String blum_median(String input) {
+    /*    privat readMatrix()
+    {
+
+    }*/
+
+//     private void input_to_integer_array()
+//     {
+//         Scanner scanner = new Scanner(input);
+//         elements = scanner.nextInt();
+// 
+//         System.out.println("Elements: " + elements);
+//         
+//         inputlist = new int[elements];
+// 
+//         /* Could loop over elements as well */
+//         for(int i = 0; i < elements; i++) {
+//             inputlist[i] = scanner.nextInt();
+//         }
+// 
+//         /* Need to track index for array anyway - better suited for Arraylist:
+//         List<Integer> list = new ArrayList<Integer>();
+//         while (scanner.hasNextInt()) {
+//             list.add(scanner.nextInt());
+//         }
+//         */        
+//     }
+// 
+//    String blum_median() {
         
-        Scanner scanner = new Scanner(input);
-
-        int []real_list;
-
-        /* Skip the first element as it is only the number of numbers */
-        scanner.nextInt();
+//        input_to_integer_array();
         
-        while (scanner.hasNextInt()) {
-            inputlist[elements] = scanner.nextInt();
-            elements++;
-        }
-        real_list = new int[elements];
-        for(int i=0; i< elements; i++) {
-            real_list[i] = inputlist[i];
-        }
-                    
-        test_listof5(listof5(real_list));
+        //    test_listof5(listof5(inputlist));
 
-        /*
-        for(int i=0; i < elements; i++) {
-            System.out.println("Noch ein Element " +inputlist[i]);
-            } */
+    //        return "";
+    //    }
 
-        return "";
-    }
+    void listsof5 () {
+        //         List<List<Int>> lists = new ArrayList<List<Individual>>(4);
 
-    int [][] listof5 (int []list) {
-        int [][]lists;
-        
-        int no_lists = (list.length)/5;
-        int cnt = 0;
+        //return lists;
 
-        /* create another list for the leftover elements */
-        if (list.length % 5 != 0) no_lists++;
-        
-        lists = new int[no_lists][5];
 
-        for(int i = 0; i < no_lists; i++) {
-            for(int j = 0; j < 5; j++) {
-                if(cnt < list.length) { /* Elements available */
-                    lists[i][j] = list[cnt];
-                    cnt++;
-                }
-            }
-        }
-
-        return lists;
+ //        List<Integer> list = new ArrayList<Integer>();        
+ //        int [][]lists;
+ //        
+ //        int no_lists = (list.length)/5;
+ //        int cnt = 0;
+ // 
+ //        /* create another list for the leftover elements */
+ //        if (list.length % 5 != 0) no_lists++;
+ //        
+ //        lists = new int[no_lists][5];
+ // 
+ //        for(int i = 0; i < no_lists; i++) {
+ //            for(int j = 0; j < 5; j++) {
+ //                if(cnt < list.length) { /* Elements available */
+ //                    lists[i][j] = list[cnt];
+ //                    cnt++;
+ //                }
+ //            }
+ //        }
+ // 
+ //        return lists;*/
     }
     void test_listof5(int [][]partitioned_lists)
     {
